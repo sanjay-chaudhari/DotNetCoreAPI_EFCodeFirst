2334e78cde77a1eb1fbda1b18ca9e8843b783c79
Chaudhari, Sanjay <sanjay.chaudhari@ecolab.com>
2021-09-27 08:06:51 +0000

    Add project files.

---
+++ Services/IEmployeeService.cs
@@ -0,0 +1,42 @@
+﻿using DotNetCoreAPI_EFCodeFirst.Models;
+using DotNetCoreAPI_EFCodeFirst.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNetCoreAPI_EFCodeFirst.Services
+{
+
+        public interface IEmployeeService
+        {
+            /// <summary>
+            /// get list of all employees
+            /// </summary>
+            /// <returns></returns>
+            List<Employees> GetEmployeesList();
+
+            /// <summary>
+            /// get employee details by employee id
+            /// </summary>
+            /// <param name="empId"></param>
+            /// <returns></returns>
+            Employees GetEmployeeDetailsById(int empId);
+
+            /// <summary>
+            ///  add edit employee
+            /// </summary>
+            /// <param name="employeeModel"></param>
+            /// <returns></returns>
+            ResponseModel SaveEmployee(Employees employeeModel);
+
+
+            /// <summary>
+            /// delete employees
+            /// </summary>
+            /// <param name="employeeId"></param>
+            /// <returns></returns>
+            ResponseModel DeleteEmployee(int employeeId);
+        }
+    }
+
